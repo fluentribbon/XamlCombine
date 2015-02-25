fae70c10f7c0bd38b8f1f7622257ac5643e6a500
batzen <batzen@gmx.org>
2015-02-25 20:46:01 +0100

    Fixing compile error regarding struct

--- src/ResourceElement.cs
+++ src/ResourceElement.cs
@@ -1,32 +1,33 @@
 ﻿namespace XamlCombine
 {
-	using System.Xml;
+    using System.Xml;
 
-	/// <summary>
-	/// Represents XAML resource.
-	/// </summary>
-	public struct ResourceElement
-	{
-		public ResourceElement(string key, XmlElement element, string[] usedKeys)
-		{
-			this.Key = key;
-			this.Element = element;
-			this.UsedKeys = usedKeys;
-		}
+    /// <summary>
+    /// Represents XAML resource.
+    /// </summary>
+    public struct ResourceElement
+    {
+        public ResourceElement(string key, XmlElement element, string[] usedKeys)
+            : this()
+        {
+            this.Key = key;
+            this.Element = element;
+            this.UsedKeys = usedKeys;
+        }
 
-		/// <summary>
-		/// Resource name.
-		/// </summary>
-		public string Key { get; private set; }
+        /// <summary>
+        /// Resource name. 
+        /// </summary>
+        public string Key { get; private set; }
 
-		/// <summary>
-		/// Resource XML node.
-		/// </summary>
-		public XmlElement Element { get; private set; }
+        /// <summary>
+        /// Resource XML node.
+        /// </summary>
+        public XmlElement Element { get; private set; }
 
-		/// <summary>
-		/// XAML keys used in this resource.
-		/// </summary>
-		public string[] UsedKeys { get; private set; }
-	}
+        /// <summary>
+        /// XAML keys used in this resource.
+        /// </summary>
+        public string[] UsedKeys { get; private set; }
+    }
 }
